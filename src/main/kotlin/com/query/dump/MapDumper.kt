6aca3edad5e0b87c2db1c75be0a57b2f2f220ab9
Mark7625 <markprobert803@gmail.com>
2023-02-05 00:13:44 +0000

    Update MapDumper.kt

--- src/main/kotlin/com/query/dump/MapDumper.kt
+++ src/main/kotlin/com/query/dump/MapDumper.kt
@@ -10,89 +10,63 @@ import com.query.utils.gzip
 import com.query.utils.progress
 import mu.KotlinLogging
 import java.io.RandomAccessFile
+import java.nio.file.Files
 
 private val logger = KotlinLogging.logger {}
 
 object MapDumper {
 
     fun init() {
-        val progress = progress("Generating Map Index", 255 * 255)
-
-        val raf = RandomAccessFile(FileUtil.getFile("dump317/","map_index").toPath().toString(), "rw")
-        var total = 0
-        raf.seek(2L)
-
-
-        for (x in 0..255) {
-            for (y in 0..255) {
-                val regionId = x shl 8 or y
-                val index = Constants.library.index(5)
-
-                val map = index.archiveId("m${x}_${y}")
-                val landscape = index.archiveId("l${x}_${y}")
-
-                if (map != -1 && landscape != -1) {
-                    raf.writeShort(regionId)
-                    raf.writeShort(map)
-                    raf.writeShort(landscape)
-                    total++
-                }
-                progress.step()
-            }
-        }
-
-        progress.close()
-
-        raf.seek(0L)
-        raf.writeShort(total)
-        raf.seek(raf.filePointer)
-        raf.close()
         val mapProgress = progress("Dumping Maps", RegionLoader.MAX_REGION)
 
-        val failedMaps = emptyMap<Int,Pair<Int,Int>>().toMutableMap()
         val missingKeys = listOf<Int>().toMutableList()
 
-        for (x in 0..255) {
-            for (y in 0..255) {
-                val regionId = getRegion(x,y)
+        val raf = RandomAccessFile(FileUtil.getFile("dump317/","map_index").toPath().toString(), "rw")
+
+        raf.seek(2L)
+        var totalMaps = 0
+        for (x in 0..256) {
+            for (y in 0..256) {
+                val regionId = x shl 8 or y
+
                 val index = Constants.library.index(5)
-                val mapID = index.archiveId("m${x}_${y}")
-                val landscapeID = index.archiveId("l${x}_${y}")
+                val objectId = index.archiveId("m${x}_${y}")
+                val landscapeId = index.archiveId("l${x}_${y}")
 
-                if (mapID != -1) {
-                    val mapData: ByteArray? = Constants.library.data(5, "m${x}_${y}")
-                    mapData?.let { gzip(FileUtil.getFile("dump317/index4/", "${mapID}.gz"),it) } ?: run {
-                        failedMaps[regionId] = Pair(mapID, landscapeID)
-                    }
-                }
+                if (objectId != -1 && landscapeId != -1) {
+                    val objects = Constants.library.data(5, "m${x}_${y}") ?: continue
+                    gzip(FileUtil.getFile("dump317/index4/", "${objectId}.gz"), objects)
 
-                if (landscapeID != -1) {
                     val keys = XteaLoader.getKeys(regionId)
 
-                    keys?.let {
-                        val landscapeData: ByteArray? = Constants.library.data(5, "l${x}_${y}", keys)
-                        landscapeData?.let { gzip(FileUtil.getFile("dump317/index4/", "${landscapeID}.gz"),it) } ?: run {
-                            failedMaps[regionId] = Pair(mapID, landscapeID)
-                        }
-                    } ?: run {
+                    val landscape = Constants.library.data(5, "l${x}_${y}", keys)
+
+                    if (landscape == null) {
                         missingKeys.add(regionId)
+                        continue
                     }
 
-                }
+                    gzip(FileUtil.getFile("dump317/index4/", "${landscapeId}.gz"), landscape)
 
+                    raf.writeShort(regionId)
+                    raf.writeShort(objectId)
+                    raf.writeShort(landscapeId)
+                    totalMaps++
+                }
                 mapProgress.step()
             }
         }
 
+        raf.seek(0L)
+        raf.writeShort(totalMaps)
+        raf.seek(raf.filePointer)
+        raf.close()
         mapProgress.close()
 
         if (missingKeys.isNotEmpty()) {
-            logger.info { "Missing Keys ${missingKeys.size}${System.lineSeparator()}$missingKeys" }
+            logger.info { "Wrong Keys ${missingKeys.size}${System.lineSeparator()}$missingKeys" }
         }
 
-        if (failedMaps.isNotEmpty()) {
-            logger.info { "Failed Maps ${failedMaps.size}${System.lineSeparator()}$failedMaps" }
-        }
 
     }
 
